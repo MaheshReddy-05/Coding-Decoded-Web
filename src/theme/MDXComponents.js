3f26650a19566f622e7a4a4b03c0a5a9aceb66e5
Mahesh Reddy <nagamaheshreddy3@gmail.com>
2023-04-22 15:53:04 +0530

    Added Plugins for Tabs, TabIteams and rfc import overried

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import SolutionAuthor from '@site/src/components/SolutionAuthor';
+import TutorialAuthors from '@site/src/components/TutorialAuthors';
+import Contributors from '@site/src/components/Contributors';
+import Table from '@site/src/components/Table';
+
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+//   custom
+  SolutionAuthor,
+  Tabs,
+  TabItem,
+  TutorialAuthors,
+  Table,
+  Contributors
+};
