55364f2b448d617e3ae877ae6be4a2b9decf0317
MaheshReddy-05 <nagamaheshreddy3@gmail.com>
2023-07-30 12:29:56 +0530

    Tailwind Config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -241,6 +241,17 @@ const config = {
       //       'https://github.com/MaheshReddy-05',
       //   }, 
       // ],
+      async function myPlugin(context, options) {
+        return {
+          name: "docusaurus-tailwindcss",
+          configurePostCss(postcssOptions) {
+            // Appends TailwindCSS and AutoPrefixer.
+            postcssOptions.plugins.push(require("tailwindcss"));
+            postcssOptions.plugins.push(require("autoprefixer"));
+            return postcssOptions;
+          },
+        };
+      },
   ],
 };
 
